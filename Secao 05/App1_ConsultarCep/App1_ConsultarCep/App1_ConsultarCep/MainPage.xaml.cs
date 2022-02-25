76739bd8cea66fd5a5563450eef0510772ac33ae
cleciosan <cleciosan@hotmail.com>
2022-02-25 17:48:53 -0300

    Criacao de App para Consultar CEP

---
+++ Secao 05/App1_ConsultarCep/App1_ConsultarCep/App1_ConsultarCep/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using App1_ConsultarCep.Services;
+using App1_ConsultarCep.Services.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1_ConsultarCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BUTTON.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Lógica do Programa
+
+
+            //TODO - Validações
+
+            string cep = CEP.Text.Trim();
+            Endereco endereco = ViaCEPService.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = string.Format("Endereco: {0}, {1} {2}", endereco.localidade, endereco.uf, endereco.logradouro);
+        }
+    }
+}
---
+++ Secao 05/App1_ConsultarCep/App1_ConsultarCep/App1_ConsultarCep/Services/ViaCEPService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCep.Services.Model;
+using Newtonsoft.Json;
+
+namespace App1_ConsultarCep.Services
+{
+    public class ViaCEPService
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return endereco;
+
+        }
+    }
+}
